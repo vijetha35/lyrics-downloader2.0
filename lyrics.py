bf6a4b9fbca64dd23ecedf8263c88e435fe769bd
vijetha35 <vijetha3594@gmail.com>
2014-01-28 21:16:45 +0530

    Create lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Name:        lyrics-downloader2.0
+# Purpose: creates a text file of the lyrics of a song
+#
+# Author:      Vijetha PV
+#
+# Created:
+# Copyright:   (c) hp
+# Licence:     
+#-------------------------------------------------------------------------------
+
+import urllib
+import re
+import sys,os,inspect
+from BeautifulSoup import BeautifulSoup
+import eyed3
+from eyed3 import mp3
+path= sys.argv[1]
+audiofile = eyed3.core.load(path)
+artist=str(audiofile.tag.artist).lower()
+s=str(audiofile.tag.title).lower()
+path = path.replace('.mp3',"")
+s =s.replace(" ","")
+artist= artist.replace(" ","")
+url='http://www.azlyrics.com/lyrics/'+artist+'/'+ s+'.html'
+
+htmltext = urllib.urlopen(url).read()
+soup =BeautifulSoup(htmltext)
+
+products= soup.findAll("div", style = "margin-left:10px;margin-right:10px;")
+
+lyrics= " ".join(str(x) for x in products)
+takeoff=['<br />','</div>','<i>','</i>','<div style="margin-left:10px;margin-right:10px;">']
+for j in takeoff:
+    lyrics=lyrics.replace(j,"\n")
+print lyrics
+with open (path+".txt","wb") as l:
+    l.write(lyrics)
+l.close()
+
